98933799982f979b6f0a33e9486db38387943255
Сергей Адышкин <adyshkinss@outlook.com>
2023-02-13 11:36:18 +0300

    Добавьте файлы проекта.

---
+++ FitnessClub/ClassHelper/EFClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FitnessClub.DB;
+using FitnessClub.Windows;
+
+namespace FitnessClub.ClassHelper
+{
+    public class EFClass
+    {
+       public static Entities context { get; set; } = new Entities();
+    }
+}
---
+++ FitnessClub/Windows/AuthWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using FitnessClub.DB;
+using FitnessClub.Windows;
+
+namespace FitnessClub.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            // авторизация
+
+            // 1. получить всех пользователей!
+            // 2. выбрать пользователей по условию 
+            // 3. из итогового списка выбрать одну запись 
+
+            //
+
+            var authUser = ClassHelper.EFClass.context.User.ToList()
+                .Where(i => i.Login == TbLogin.Text && i.Password == TbPassword.Text)
+                .FirstOrDefault();
+
+            if (authUser != null)
+            { 
+                // переход на нужное окно
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Пользователь не найден");
+            }
+        }
+    }
+}
